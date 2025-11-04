c2a27cab4e6282da6aa6f1802c9f38421760e705
Blake Vogler <blake.a.vogler@gmail.com>
2025-11-03 20:37:31 -0600

    Python_1

--- booleans-and-conditionals/exercises01.py
+++ booleans-and-conditionals/exercises01.py
@@ -1,6 +1,12 @@
 # Assign the variables for exercise 1 here:
+from operator import truediv
 
-
+engine_indicator_light = "red blinking"
+space_suits_on = True
+shuttle_cabin_ready = True
+crew_status = space_suits_on and shuttle_cabin_ready
+computer_status_code = 200
+shuttle_speed = 15000
 
 # BEFORE running the code, predict what will be printed to the console by the following statements:
 
@@ -10,3 +16,6 @@ elif engine_indicator_light == "green blinking":
   print("engines are preparing to start")
 else:
   print("engines are off")
+
+  #If the engine light is green - start the engine. Else if the engine light is green blinking, pring 'engines are
+  #preparing to start. If neither (is red) then print 'engines are off'. Which it will print engines are off.
